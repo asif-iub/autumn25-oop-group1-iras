08d19f028a60c7a8c9d35519346d8a4e8fa4ac9c
Asif Mahmood <asif.mahmood@iub.edu.bd>
2026-04-05 12:03:04 +0600

    Added login code with default username and password

--- src/main/java/com/group1/iras/LoginController.java
+++ src/main/java/com/group1/iras/LoginController.java
@@ -3,6 +3,8 @@ package com.group1.iras;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 
+import java.io.IOException;
+
 public class LoginController
 {
     @javafx.fxml.FXML
@@ -17,6 +19,14 @@ public class LoginController
     }
 
     @javafx.fxml.FXML
-    public void login(ActionEvent actionEvent) {
+    public void login(ActionEvent actionEvent) throws IOException {
+        if (usernameField.getText().equals("asif")
+                && passwordField.getText().equals("1234")
+        ) {
+            label.setText("Log in successful");
+            SceneSwitcher.switchTo("/com/group1/iras/mahmood/user3dashboard.fxml");
+        }
+        else
+            label.setText("Log in failed");
     }
 }
