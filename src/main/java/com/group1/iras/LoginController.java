3afbe3a44754ed3e8011a5522fe08306d36b45d7
Asif Mahmood <asif.mahmood@iub.edu.bd>
2026-04-05 11:52:01 +0600

    Added login scene

---
+++ src/main/java/com/group1/iras/LoginController.java
@@ -0,0 +1,22 @@
+package com.group1.iras;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class LoginController
+{
+    @javafx.fxml.FXML
+    private TextField usernameField;
+    @javafx.fxml.FXML
+    private PasswordField passwordField;
+    @javafx.fxml.FXML
+    private Label label;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void login(ActionEvent actionEvent) {
+    }
+}
