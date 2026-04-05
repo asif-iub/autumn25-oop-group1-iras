4977b3c4faca89b3c9185fc19fe2318b32bcf8d6
Asif Mahmood <asif.mahmood@iub.edu.bd>
2026-04-05 12:05:42 +0600

    Fixed scene switching issue from login to user 3 dashboard

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,7 @@ module com.group1.iras {
 
     opens com.group1.iras to javafx.fxml;
     exports com.group1.iras;
+
+    opens com.group1.iras.mahmood to javafx.fxml;
+    exports com.group1.iras.mahmood;
 }
